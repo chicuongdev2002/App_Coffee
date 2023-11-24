6efe47d125039be933b158afea541923f886025a
chicuongdev2002 <124854803+chicuongdev2002@users.noreply.github.com>
2023-11-25 01:59:37 +0700

    Update Login

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,80 +1,117 @@
-import React,{ useState, useRef } from "react";
-import { ImageBackground, StyleSheet, View, Image, Text, TextInput,Pressable,TouchableOpacity } from "react-native";
-import logo from '../image/logo/logo.png'
-import covn from '../image/logo/covn.png'
-import fb from '../image/logo/fb.png'
-import apple from '../image/logo/apple.png'
-import google from '../image/logo/google.png'
-import MainApp from "./MainApp";
-import Home from "./Home";
+import React, { useState, useRef } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View ,TextInput} from "react-native";
 import PhoneInput from "react-native-phone-number-input";
-import CountryPicker, { DARK_THEME } from 'react-native-country-picker-modal'
+import logo from '../image/logo/logo.png';
+import apple from '../image/logo/apple.png';
+import fb from '../image/logo/fb.png';
+import google from '../image/logo/google.png';
 import { sendSmsVerification } from "../api/verify";
-// import {sendSmsVerification} from "../api/verifysms"
+
 const Login = ({ navigation }) => {
   const [value, setValue] = useState("");
- const [formattedValue, setFormattedValue] = useState("");
- const phoneInput = useRef(null);
- const onPressFlag = () => {
-  phoneInput.current.selectCountry();
-};
+  const [formattedValue, setFormattedValue] = useState("");
+  const [otp, setOTP] = useState('110402');
+  const [otpInput, setOTPInput] = useState('');
+  const [showOTPInput, setShowOTPInput] = useState(false);
+  const phoneInput = useRef(null);
+
+  const onPressFlag = () => {
+    phoneInput.current.selectCountry();
+  };
+
+  const handleSendOTP = () => {
+    const twilioFunctionUrl = 'https://hc-5776.twil.io/sendsms';
+    fetch(twilioFunctionUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        to: formattedValue,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.otp);
+         setOTP(data.otp);
+        setShowOTPInput(true);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const handleConfirmOTP = () => {
+    if (otpInput.trim() === otp.trim()) {
+      navigation.navigate("MainApp");
+    } else {
+      console.log("Mã OTP không đúng");
+    }
+  };
+
   return (
     <View style={styles.container}>
-       <View>
-        <Image source={logo} style={styles.logo}></Image>
-        </View>
-        <View style={styles.containerText}>
-          <Text style={styles.styleText}>Bắt đầu cuộc hành trình của bạn</Text>
-        </View>
-        <PhoneInput
-           ref={phoneInput}
-           defaultValue={value}
-           defaultCode="VN"
-           layout="first"
-           onChangeText={(text) => {
-             setValue(text);
-           }}
-           onChangeFormattedText={(text) => {
-             setFormattedValue(text);
-           }}
-           countryPickerProps={{ withAlphaFilter: true }}
-           withShadow
-           autoFocus
-           onPressFlag={onPressFlag}
-           
-         />
-       <View style={styles.buttonTiepTuc}>
-       <TouchableOpacity
-           style={styles.button}
-           onPress={() => {
-            sendSmsVerification(formattedValue).then((sent) => {
-              navigation.navigate("MainApp", { phoneNumber: formattedValue });
-              console.log(formattedValue);
-              });              
-            }}
-         >
-           <Text style={styles.buttonText}>Tiếp tục</Text>
-         </TouchableOpacity>
-       </View>
-       <View style={styles.containerFotter}>
-        <Image source={apple} style={styles.imageFooter}></Image>
-        <View style={styles.fb} >
-        <Image source={fb} style={styles.imageFooter}></Image>
+      <View>
+        <Image source={logo} style={styles.logo} />
+      </View>
+      <View style={styles.containerText}>
+        <Text style={styles.styleText}>Bắt đầu cuộc hành trình của bạn</Text>
+      </View>
+      <PhoneInput
+        ref={phoneInput}
+        defaultValue={value}
+        defaultCode="VN"
+        layout="first"
+        onChangeText={(text) => setValue(text)}
+        onChangeFormattedText={(text) => setFormattedValue(text)}
+        countryPickerProps={{ withAlphaFilter: true }}
+        withShadow
+        autoFocus
+        onPressFlag={onPressFlag}
+      />
+      <TouchableOpacity
+        style={styles.buttonTiepTuc}
+        onPress={handleSendOTP}
+        setShowOTPInput={true}
+      >
+        
+        <Text style={styles.buttonText}>Tiếp tục</Text>
+      </TouchableOpacity>
+        <>
+          <TextInput
+            style={styles.otpInput}
+            placeholder="Nhập mã OTP"
+            keyboardType="numeric"
+            onChangeText={(text) => setOTPInput(text)}
+            value={otpInput}
+          />
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleConfirmOTP}
+          >
+            <Text style={styles.buttonText}>Xác nhận OTP</Text>
+          </TouchableOpacity>
+        </>
+      
+      <View style={styles.containerFotter}>
+        <Image source={apple} style={styles.imageFooter} />
+        <View style={styles.fb}>
+          <Image source={fb} style={styles.imageFooter} />
         </View>
         <View style={styles.google}>
-        <Image source={google} style={styles.imageFooter}></Image>
+          <Image source={google} style={styles.imageFooter} />
         </View>
-       </View>
-       <View style={styles.footer}>
-        <TouchableOpacity onPress={()=>navigation.navigate("MainApp")}> 
-           <Text style={styles.textFooter}>TIẾP TỤC NHƯ KHÁCH</Text>
-           </TouchableOpacity>
-       </View>
-       <View style={styles.footer2}>
-        <Text>Bạn đã có tài khoản?</Text>
-        <Text style={{color:"#AA0000",fontWeight:700}}> Đăng nhập</Text>
-       </View>
       </View>
+      <View style={styles.footer}>
+        <TouchableOpacity onPress={() => navigation.navigate("MainApp")}>
+          <Text style={styles.textFooter}>TIẾP TỤC NHƯ KHÁCH</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.footer2}>
+        <Text>Bạn đã có tài khoản?</Text>
+        <Text style={{ color: "#AA0000", fontWeight: 700 }}> Đăng nhập</Text>
+      </View>
+    </View>
   );
 };
 
@@ -85,99 +122,82 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#f0e2b1",
     width: "100%",
-    height: "100%"
+    height: "100%",
   },
-  styleText:{
-    color:"#4c2f16",
-    fontSize:30,
-    fontFamily:"Roboto",
-    fontStyle:'normal',
-    fontWeight:'700',
-  },
-  logo:{
-    width: 200,
-    height: 200,
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  covnImage: {
-    width: 40,
-    height: 40,
-  },
-  plus84Text: {
+  styleText: {
     color: "#4c2f16",
-    fontSize: 20,
+    fontSize: 30,
     fontFamily: "Roboto",
     fontStyle: 'normal',
     fontWeight: '700',
-    marginRight: 10,
   },
-  leftSection: {
-    flex: 3,
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#FFFFFF",
-    justifyContent: 'center',
-    borderRadius: 5,
+  logo: {
+    width: 200,
+    height: 200,
   },
-  rightSection: {
-    flex: 7,
-    backgroundColor: "#FFFFFF",
-    width: 250,
-    height: 60,
-    borderRadius: 5,
-  },
-  phoneNumberInput: {
-    flex: 1,
-  },
-  containerText:{
-    marginTop:10,
-    marginBottom:50,
+  containerText: {
+    marginTop: 10,
+    marginBottom: 50,
     justifyContent: 'center',
     alignContent: 'center',
     alignItems: 'center',
   },
-  textTiepTuc:{
-
-  },
-  buttonTiepTuc:{
-    backgroundColor:"#DDDDDD",
-    width:350,
-    height:50,
-    alignItems:'center',
+  buttonTiepTuc: {
+    backgroundColor: "#DDDDDD",
+    width: 350,
+    height: 50,
+    alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 5,
     marginTop: 20,
   },
-  imageFooter:{
+  imageFooter: {
     width: 50,
     height: 50,
   },
-  containerFotter:{
+  containerFotter: {
     flexDirection: 'row',
     marginTop: 10,
   },
-  google:{
+  google: {
     marginTop: 5,
   },
-  fb:{
-    marginLeft:10
+  fb: {
+    marginLeft: 10
   },
-  textFooter:{
-    fontFamily:"Roboto",
-    fontSize:15,
-    fontWeight:"700",
-    color:"#AA0000"
+  textFooter: {
+    fontFamily: "Roboto",
+    fontSize: 15,
+    fontWeight: "700",
+    color: "#AA0000"
   },
-  footer:{
+  footer: {
     marginTop: 20,
   },
-  footer2:{
+  footer2: {
     flexDirection: 'row',
     marginTop: 150,
-  }
+  },
+  otpInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: "#DDDDDD",
+    width: 350,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
 
 export default Login;
